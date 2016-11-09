239997ec427da8513b94c00946548db626a37ec9
Adam Goldman <adamgoldman7@gmail.com>
2016-11-09 13:00:58 +0200

    (scripts): replace mocha test with istanbul coverage

--- scripts/gitHooks.sh
+++ scripts/gitHooks.sh
@@ -3,4 +3,4 @@ rm -f .git/hooks/pre-commit
 touch .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
 echo npm run lint >> .git/hooks/pre-commit
-echo npm test >> .git/hooks/pre-commit
+echo npm run coverage >> .git/hooks/pre-commit
